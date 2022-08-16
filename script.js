6d1ce471e225ae6970e2adccce5f1a09c1164387
Bhavika Mevada <bmevada>
2022-08-16 22:59:37 +1000

    added array string characters

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
 // Generate a random password
-function newpassword(){
+function newpassword() 
   
-    //Set password length and complexity
-    document.getElementById("password").value = password
+//Prompt usert for password length and complexity
+document.getElementById("Generate-Password").value = password
 
-    //Set the length and possible values of the password
-    var charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()_+=-{}:<>?[];',./";
-    min passwordlength = 8;
-    max passwordlength = 128
-    var password = " ";
+var length = Number(prompt("Enter a password length between 8 and 128"));
+        charType = prompt("Enter a character type: special, numeric, uppercase, lowercase");
+        password = Generate-Password();
+        document.getElementById("displaypassword").value = password;
+    
+        var password_strings = {
+        lowercase: 'abcdefghijklmnopqrstuvwxyz',
+        uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        numeric: '0123456789',
+        special: " !"@$%#\'()*+,-./:;<=>?@[]^_`{|}~"
+        };
+
+    // var passwordlength = 7;
+    // var minPasswordLength = 8;
+    // var maxPasswordLength = 128;
+    // var password = " ";
+
+    var length = window.prompt("Enter a password length between 8 and 128");
+
+    var lowercase = window.confirm("Do you want the password to include lowercase letters?");
+    if (lowercase) {
+      passwordCharSet += password_strings.lowercase;
+    };
+  
+    var uppercase = window.confirm("Do you want the password to include uppercase letters?");
+    if (uppercase) {
+      passwordCharSet += password_strings.uppercase;
+    };
+  
+    var symbols = window.confirm("Do you want the password to include symbols?");
+    if (symbols) {
+      passwordCharSet += password_strings.symbol;
+    };
+  
+    var numbers = window.confirm("Do you want the password to include numbers?");
+    if (numbers) {
+      passwordCharSet += password_strings.number;
+    };
 
     //Create random password characters
     for (var i=0; i<passwordlength; i++){
-        var randomvalue = numerical.charat(Math.floor(Math.random) * Math.floor);
+        var randomvalue = charset(Math.floor(Math.random) * Math.floor);
         password +=chars.substring(randomvalue, randomvalue+1);
     }
 
     //Display password in textbox
     document.getElementById("Generate-Password").value = password;
 
-}
+
+document.getElementById("btn").addEventListener("click", newpassword)
