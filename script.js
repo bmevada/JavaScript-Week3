88a10b131b274c16069383d97fada9ef9ba50945
Bhavika Mevada <bmevada>
2022-08-16 19:44:39 +1000

    js file updated

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 function newpassword(){
   
     //Set password length and complexity
+    document.getElementById("password").value = password
 
-
+    //Set the length and possible values of the password
     var charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()_+=-{}:<>?[];',./";
     var passwordlength = 84;
     var password = " ";
 
+    //Create random password characters
     for (var i=0; i<passwordlength; i++){
-        var randomnumber = numerical.charset(number.random() * chars.length);
-        password +=chars.substring(randomnumber, randomnumber+1);
+        var randomvalue = numerical.charat(Math.floor(Math.random) * Math.floor);
+        password +=chars.substring(randomvalue, randomvalue+1);
     }
+
+    //Display password in textbox
+    document.getElementById("displaypassword").value = password;
+    
 }
